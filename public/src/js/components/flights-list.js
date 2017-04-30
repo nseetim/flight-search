87e2a0ae1f72e066f0688083183984ec566fd9aa
Tiago Engel <tiagohngl@gmail.com>
2017-04-30 17:07:14 -0300

    Fix flight field names and format departure and arrival times

--- public/src/js/components/flights-list.js
+++ public/src/js/components/flights-list.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 
-function displayTime(timeInMinutes) {
+function durationDisplay(timeInMinutes) {
   let displayStr = '';
   const hours = Math.floor(timeInMinutes / 60);
   if (hours > 0) {
@@ -10,28 +10,40 @@ function displayTime(timeInMinutes) {
   return `${displayStr}${minutes}min`;
 }
 
+function timeDisplay(dateTime) {
+  const pl = (val) => {
+    const str = `${val}`;
+    return `${'00'.substring(0, 2 - str.length)}${str}`;
+  };
+  return `${pl(dateTime.getHours())}:${pl(dateTime.getMinutes())}`;
+}
+
 export default function render(flights) {
   const ul = $('<ul>', { class: 'flights-list' });
   flights.forEach((flight) => {
     const li = `
       <li class="columns is-mobile">
         <div class="column is-9">
-          <h3 class="flights-list__flight-title">${flight.airline} <small>(${flight.seatType})</small></h3>
+          <h3 class="flights-list__flight-title">${flight.airline.name} <small>(${flight.seatType})</small></h3>
           <div class="columns">
-            <div class="column">
-              <p class="label">Departure</p>
-              <strong>${flight.departure.time}</strong> ${flight.departure.airport}
+            <div class="column is-2">
+              <p class="label">Flight Num.</p>
+              ${flight.flightNum}
             </div>
-            <div class="column">
+            <div class="column is-5">
+              <p class="label">Departure</p>
+              <strong>${timeDisplay(flight.start.dateTime)}</strong> ${flight.start.airportName}
+            </div>
+            <div class="column is-5">
               <p class="label">Arrival</p>
-              <strong>${flight.arrival.time}</strong> ${flight.arrival.airport}
+              <strong>${timeDisplay(flight.finish.dateTime)}</strong> ${flight.finish.airportName}
             </div>
           </div>
         </div>
         <div class="column is-3 has-text-right">
           <h2 class="flights-list__flight-price">$${flight.price}</h2>
-          <p class="label">Flight Time</p>
-          <p><strong>${displayTime(flight.totalTime)}</strong></p>
+          <p class="label">Flight Duration</p>
+          <p><strong>${durationDisplay(flight.durationMin)}</strong></p>
         </div>
       </li>
       <hr />`;
